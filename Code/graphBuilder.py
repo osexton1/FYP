e67faed95dc06507a02f7a4513870a0e5ea148d4
Odhran Sexton <119361266@umail.ucc.ie>
2023-04-23 17:33:19 +0100

    Updated to randomly generate Legal and Illegal paths

--- Code/graphBuilder.py
+++ Code/graphBuilder.py
@@ -1,28 +1,51 @@
 import random
 
-num_nodes = 25
-num_edges = 50
-existing_edges = set()
+def graphBuilder(num_nodes, num_edges):
+    num_nodes = num_nodes
+    num_edges = num_edges
+    existing_edges = set()
 
-graph = open('graph50.txt', 'w')
+    graph = open('graphExp.txt', 'w')
 
-graph.write(f"graph [\n\tcomment \"This is a graph of {num_nodes} nodes\"\n\tdirected 1\n\tIsPlanar 1\n")
+    graph.write(f"graph [\n\tcomment \"This is a graph of {num_nodes} nodes\"\n\tdirected 1\n\tIsPlanar 1\n")
 
-for i in range(1, num_nodes+1):
-    graph.write(f"\tnode [\n\t\tid {i}\n\t\tlabel\n\t\t\"Node {i}\"\n\t]\n")
+    for i in range(1, num_nodes+1):
+        graph.write(f"\tnode [\n\t\tid {i}\n\t\tlabel\n\t\t\"Node {i}\"\n\t]\n")
 
-for i in range(num_edges):
-    source = random.randint(1, num_nodes)
-    target = random.randint(1, num_nodes)
-    while source == target or (source, target) in existing_edges:
+    for i in range(num_edges//2):
+        source = random.randint(1, num_nodes)
         target = random.randint(1, num_nodes)
-    weight = random.randint(1, 20)
-    included = 1
-    graph.write(f"\tedge [\n\t\tsource {source}\n\t\ttarget {target}\n\t\tweight {weight}\n\t\tincluded {included}\n\t]\n")
-    graph.write(f"\tedge [\n\t\tsource {target}\n\t\ttarget {source}\n\t\tweight {weight}\n\t\tincluded {included}\n\t]\n")
-    existing_edges.add((source, target))
-    existing_edges.add((target, source))
+        while source == target or (source, target) in existing_edges:
+            target = random.randint(1, num_nodes)
+        weight = random.randint(1, 20)
+        included = 1
+        graph.write(f"\tedge [\n\t\tsource {source}\n\t\ttarget {target}\n\t\tweight {weight}\n\t\tincluded {included}\n\t]\n")
+        graph.write(f"\tedge [\n\t\tsource {target}\n\t\ttarget {source}\n\t\tweight {weight}\n\t\tincluded {included}\n\t]\n")
+        existing_edges.add((source, target))
+        existing_edges.add((target, source))
 
-graph.write("]")
+    graph.write("]")
 
-graph.close()
+    graph.close()
+
+    paths = open('paths.txt', 'w')
+
+    max_restrictions = (num_nodes*4)//10
+    paths_assigned = []
+    for i in range(max_restrictions):
+        if i < max_restrictions//2:
+            source = random.randint(1, num_nodes)
+            target = random.randint(1, num_nodes)
+            while source == target or (source, target) in paths_assigned:
+                target = random.randint(1, num_nodes)
+            paths.write(f"Legal, Node {source}, Node {target}\n")
+            paths_assigned.append((source, target))
+        else:
+            source = random.randint(1, num_nodes)
+            target = random.randint(1, num_nodes)
+            while source == target or (source, target) in paths_assigned:
+                target = random.randint(1, num_nodes)
+            paths.write(f"Illegal, Node {source}, Node {target}\n")
+            paths_assigned.append((source, target))
+
+    paths.close()
